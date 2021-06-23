fc3814e9d0f6eb7d6819035712f9627bd567800a
GazApp <juangaz@hotmail.com>
2021-06-23 18:02:51 -0500

    EXPRESSIONES

---
+++ 07 - Expressions.py
@@ -0,0 +1,20 @@
+"""
+En Python una expression es culquier cosa que pueda calcularse para,
+develover un valor para la linea
+"""
+
+a = 12
+b = 3
+
+print(a + b)  # 15
+print(a - b)  # 9
+print(a * b)  # 36
+print(a / b)  # 4.0
+print(a // b)  # 4 Integer divison, rounded down towards minus infinity "DIVISON ENTERA"
+print(a % b)  # 0 modulo: the remainder after integer divison
+
+"""
+TODO EL CODIGO ANTERIOR SON EXPRESIONES, POR SE PUEDEN CLACULAR Y EN 
+EL CASO DE LA LINEA 6 Y 7, TIENEN EL SIGNO DE IGUAL YA ESTA ESPERANDO 
+O SE LE STA ASIGNADO UN VALOR, ES POR ESTO QUE TAMBIEN ES UNA EXPRESSION.
+"""
