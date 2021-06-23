0394e53b66f80fe8b4f9688144c37b6ca46e24ba
GazApp <juangaz@hotmail.com>
2021-06-23 17:23:18 -0500

    Tipos de Datos

---
+++ 05 - Numeric Data Types in Python.py
@@ -0,0 +1,15 @@
+# *** PYTHON TYPES DATES ***
+# Python  has several built-in data types,
+# can be  classed as:
+#
+# 1. NUMERIC
+     # INT
+     # FLOAT
+     # COMPLEX
+     # DECIMAL
+# 2. ITERATOR
+# 3. SEQUENCE (Which are also iterators)
+# 4. MAPPING
+# 5. File
+# 6. CLASS
+# 7. EXCEPTION
