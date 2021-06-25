eea0c934269c3b144b20366cb92efab1da421155
GazApp <juangaz@hotmail.com>
2021-06-25 17:47:12 -0500

    Indexacion de Strings (-)

---
+++ 10 - Negative Indexing in Strings.py
@@ -0,0 +1,13 @@
+a = 'Blue_Demon'
+
+print(a)
+
+print(a[-5])
+
+print(type(a))
+# Imprime la indexacion de longitud de la cadena, de Derecha a Izquierda
+print(a[-1])
+
+print('----------->')
+print(a[3 -5])
+#
