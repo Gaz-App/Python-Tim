2f3a614f85489db9bcaadde28e45b2369b5df046
GazApp <juangaz@hotmail.com>
2021-06-25 18:27:21 -0500

    Segmetacion de Cadenas

---
+++ 11 - Slicing.py
@@ -0,0 +1,16 @@
+a = 'Blue Demon'
+
+print(a[0:4])
+
+# Va a cortar del index 0 al Index 4
+# otra forma de hacerlo:
+
+print(a[:4])
+
+print(a[4:])
+
+print (a[4:] + " " + a[:4])
+
+print ("------------>")
+
+print(a[:])
