4a449dfd2d45091b0b11b39afc80d3ce7680265d
GazApp <juangaz@hotmail.com>
2021-06-28 18:28:54 -0500

    Segmetacion de Cadenas negativas

---
+++ 12 - Slicing with Negative Numbers.py
@@ -0,0 +1,9 @@
+a = "Norwegian Blue"
+
+print(a[0:6])    # Norweg corta de 0 al 6
+print (a[-14:-8]) # Norweg
+print(a[-4:-1])
+# Blu corta del -4 (izq)= B y despues -2 izq. de Blue = l
+
+print(a[-4:13])
+# Blu corta - 4 izq = Blue u despues 12 a la derecha: Bl
