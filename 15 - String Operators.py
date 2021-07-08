28dcb17a9593e771394403828e7cdf2a26f26057
GazApp <juangaz@hotmail.com>
2021-07-08 17:40:54 -0500

    Operdadores de Cadena

---
+++ 15 - String Operators.py
@@ -0,0 +1,35 @@
+"""
+- Pyhon 3.9:  Has 5 sequences types built in:
+1. The String Type.
+2. List
+3. Tuple.
+4. Range.
+5. Bytes and Bytearray
+
+A sequence is an ordered collection of objects
+Sequence Types — list, tuple, range
+
+"""
+
+s1 = 'JUAN '
+s2 = 'FRANCISCO '
+s3 = 'GAZ '
+s4 = 'CRESPO'
+print(s1+s2+s3+s4)
+
+print (s3 * 5)
+
+#print (s3 * 5 +4) ->va a marcar un porque no es valido la oparacion
+print (s3 * (5 + 4))
+print('-------------------->')
+name ='francisco'
+#para evaular una condcion de verdadero o falso:
+print ('fra'in name)
+print ('gaz' in name)
+print ('cisco' in name)
+print ('cres' in name)
+print ('CISCO' in name)
+
+
+#listas = ['a', 'b', 'c', 1, 2, 3]
+#print(listas)
